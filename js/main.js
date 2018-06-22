7eefa6364a5fa960d9098295b6e1d5c3a5be482f
Thais Cabral <thaiscblima@gmail.com>
2018-06-21 20:27:24 -0400

    New functions

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// alert('Welcome!')
+
+function addNums(){
+    // fuction body
+    var num1 = document.getElementById(num1).value;
+    var num2 = document.getElementById(num2).value;
+    console.log('num1') // output data to my console
+    console.log('num2')
+
+    document.getElementById('result').innerHTML = (parseInt(num1) + parseInt(num2));
+}
+
+function change(){
+    document.getElementById('foo').innerHTML = '<p>You change me</p>';
+}
+
+console.log('welcome');
+console.log(5);
+console.log(false);
+
+console.log(3 + 3);
+
+var name = 'Lucille';
+var age = 30;
+var isCodingFun = true;
+
+/* Basic Data Types in JS
+
+strings 'foo'
+numbers 5, 5000
+booleans, true or false
+
+
+*/
